0904f8fa6fd2f99b527a522888007962dfc25229
Saran K <hiclashersaran@gmail.com>
2026-07-08 09:32:32 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# 🎫 GitHub Repository Counter Badge
+This is a GitHub-Badge SVG graphic which shows the number of repository of a user.
+The counter gets automatically updated by a [GitHub CI workflow](https://resources.github.com/ci-cd/).
+
+![github-repo-count](https://raw.githubusercontent.com/TobseF/github-badge/master/github-repo-count.svg)
+
+## ⭐ Features
+ ⭐ Customizable  
+ ⭐ SVG graphic  
+ ⭐ Always up-to-date  
+ ⭐ Image hosted by GitHub  
+ ⭐ Git-based stats history  
+ ⭐ No API key is needed
+
+## 📖 How it works
+This `update-badge-script.js` [Node.js](https://nodejs.org/en/) script reads 
+the [GitHub user API](https://docs.github.com/en/rest/users/users)
+and writes the badge stats into an SVG template file (`github-repo-count-template.svg`).
+If the generated badge is newer then the file in the repository, it
+commits the new generated `github-repo-count.svg` to this GIT repository.
+The pipeline gets triggered by a cron job, which automatically updates the image every midnight.
+
+You can link the generated file by:  
+https://raw.githubusercontent.com/$userName$/github-badge/master/github-repo-count.svg (replace `$userName$`)
+
+## 🛠 Config
+The script can be configured to generate a badge for any user:
+* `userName`: Your username on GitHub.  
+   Example url: https://github.com/TobseF  
+   `userName` = `TobseF`
+* `cron`: You can change the update time interval in the `.github/workflows/main.yml`:  
+   On Line 6:  
+   `- cron: '0 0 * * *'` (every day at midnight).
+   Uses the [cron-job syntax](https://crontab.guru/every-midnight).
+
---
+++ update-badge-script.js
@@ -0,0 +1,74 @@
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+let userName = "tobsef";
+
+let pipelineRun = true; // Set to false for local debugging
+let templateFile = 'github-repo-count-template.svg';
+let outputFile = 'github-repo-count.svg';
+let apiURL = "https://api.github.com/users/";
+let repoCountUrl = apiURL + userName;
+
+fetch(repoCountUrl, {
+    method: 'get',
+    headers: {'Content-Type': 'application/json'}
+})
+    .then((res) => res.json())
+    .then((json) => {
+        updateBadge(json.public_repos);
+    });
+
+function updateBadge(repoCount) {
+    try {
+        console.log("Received count of " + repoCount + " repos from GitHub API");
+        let templateData = readFile(templateFile);
+        let compiledBadge = compileTemplate(templateData, repoCount);
+        let oldBadge = readFile(outputFile);
+
+        if (oldBadge === compiledBadge) {
+            console.log("Badge data has not changed. Skipping commit.");
+            setUpdateBannerEnv("false");
+        } else {
+            console.log("Updating badge ...");
+            fs.writeFileSync("./" + outputFile, compiledBadge);
+            console.log("Updated " + outputFile + " successfully");
+            setUpdateBannerEnv("true")
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function setUpdateBannerEnv(value) {
+    setEnv("update-badge", value);
+}
+
+function setEnv(key, value) {
+    if (pipelineRun) {
+        fs.writeFileSync(process.env.GITHUB_ENV, key + "=" + value);
+    } else {
+        console.log("New Property: " + key + "=" + value);
+    }
+}
+
+function readFile(file) {
+    return fs.readFileSync("./" + file, 'utf8');
+}
+
+function compileTemplate(template, repoCount) {
+    let compiled = setTemplateVar(template, "repoCount", repoCount);
+    return setTemplateVar(compiled, "length", calculateSize(repoCount));
+}
+
+function setTemplateVar(template, name, value) {
+    return template.replaceAll("${" + name + "}", value);
+}
+
+function calculateSize(number) {
+    let letterSize = 80;
+    return size(number) * letterSize;
+}
+
+function size(number) {
+    return Math.abs(number).toString().length;
+}
